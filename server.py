3c8e7b6e796701d16780e13cd521af3a861f56e7
Taylor Amery <68263125+taylor-amery@users.noreply.github.com>
2025-03-23 20:34:02 -0700

    Update server.py
    more debugging attemtps

--- server.py
+++ server.py
@@ -31,7 +31,7 @@ def fetch_trial_data(nct_id):
         return None
 
 
-@app.route('/ctgov-trial-fetch.html', methods=['GET'])
+@app.route('/', methods=['GET'])
 def get_trial():
     nct_number = request.args.get('nct')
     if not nct_number:
