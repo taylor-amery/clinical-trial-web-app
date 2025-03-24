e51ea5f775c0fce9e4247a4a54ea716b974874bb
Taylor Amery <68263125+taylor-amery@users.noreply.github.com>
2025-03-23 19:14:21 -0700

    Update server.py
    Update code to use Render's port

--- server.py
+++ server.py
@@ -22,6 +22,9 @@ def get_trial():
     
     data = fetch_trial_data(nct_number)
     return jsonify(data)
+import os
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
